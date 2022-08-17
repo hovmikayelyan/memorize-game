4ff6ba634d5c154b5605afa2b22dd21f4940e4c0
Hovhannes Mikayelyan <hovmikayelyan@gmail.com>
2022-08-17 16:38:42 +0400

    CHange ContentView

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,10 +9,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        return RoundedRectangle(cornerRadius: 20).stroke(lineWidth: 3).padding(.horizontal).foregroundColor(.red)
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
